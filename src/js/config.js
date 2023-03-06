a99ed956473280f1f306248b153a90531c9df789
May7331 <purpleseed1107@gmail.com>
2023-03-06 11:26:38 +0800

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RESULTS_PER_PAGE = 10;
+export const KEY = '8acdfd89-2695-4270-88d1-da0d66eb30de';
+export const MODAL_CLOSE_SEC = 2.5;
